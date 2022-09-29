66f48e9a1371037b567079035238a79d24d9f5f0
Fabricio <f.sillva2507@gmail.com>
2022-09-29 19:43:33 -0300

    usando setinterval

--- aula01/teste.js
+++ aula01/teste.js
@@ -2,15 +2,24 @@
 //Importando o módulo os do node e colocando em uma variavel
 const os = require('os')
 
+setInterval(() => {
 
-const totalMem = parseInt(os.totalmem()/1024 /1024)
-const freeMem = parseInt(os.freemem()/1024/1024)
+    const totalMem = parseInt(os.totalmem() / 1024 / 1024)
+    const freeMem = parseInt(os.freemem() / 1024 / 1024)
+    const percent = parseInt((freeMem / totalMem) * 100)
+   
+    const memoria = {
+        total: `${totalMem}MB`,
+        free: `${freeMem} MB`,
+        percent: `${percent} %`
+    }
+    console.clear()
+    console.table(memoria)
 
-const memoria = {
-    total:`${totalMem}MB` ,
-    free: `${freeMem}`
-}
-console.clear()
-console.table(memoria)
+}, 1000)
+
+
+
+
 
 // console.log(`Memória Livre: ${freeMem}`,`Total de Memória: ${totalMem}`)
