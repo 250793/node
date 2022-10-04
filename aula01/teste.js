1d8d2721e9f3419658f20c2a01d8114f814c975e
Fabricio <f.sillva2507@gmail.com>
2022-10-04 20:54:45 -0300

    criando server

--- aula01/teste.js
+++ aula01/teste.js
@@ -1,21 +1,35 @@
 //CommonJS - prover o import e export os módulos
 //Importando o módulo os do node e colocando em uma variavel
 const os = require('os')
+const { byteToGB } = require('../aula02/convertUnitStorage')
+const fs = require('fs')
 
 setInterval(() => {
 
     const totalMem = parseInt(os.totalmem() / 1024 / 1024)
     const freeMem = parseInt(os.freemem() / 1024 / 1024)
     const percent = parseInt((freeMem / totalMem) * 100)
-   
+
     const memoria = {
         total: `${totalMem}MB`,
         free: `${freeMem} MB`,
-        percent: `${percent} %`
+        percent: `${percent} %`,
+        totalgb: `${byteToGB(os.totalmem())}`,
+        freegb: `${byteToGB(os.freemem())}`
+
+
+
+
     }
+    let dateTime = new Date().toLocaleString();
+    let print = `${dateTime} ${JSON.stringify(memoria)}\n`
     console.clear()
     console.table(memoria)
-
+    fs.appendFile(
+        './aula02/log.txt',
+        print,
+        'utf-8',
+        () => console.log('Log inserido'))
 }, 1000)
 
 
---
+++ aula03/server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+
+
+
+http.createServer((req, res)=> {
+    console.log(req, res)
+}).listen(5000 , () => console.log('Servindo funcionando...'))
